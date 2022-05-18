4441d28723267bf4f1d4cb3073e428128e60fb57
tjcoppola234 <37402171+tjcoppola234@users.noreply.github.com>
2022-05-18 12:25:44 -0400

    layout changes

--- src/render.js
+++ src/render.js
@@ -1,9 +1,17 @@
-function submitClicked() {
+function collPicked() {
+  hideAddItemBox();
+  document.getElementById('collectionItems').innerHTML = '';
   if (document.getElementById('collectionSelect').value === 'soda') {
-    if (document.getElementById('pics').style.display === 'none') {
-      document.getElementById('pics').style.display = '';
-    } else {
-      document.getElementById('pics').style.display = 'none';
-    }
+    document.getElementById('collectionItems').innerHTML +=
+        `<img src="../resources/assets/logo.png">
+         <img src="../resources/assets/logo.png">`;
   }
+  document.getElementById('addItemButton').removeAttribute('disabled');
+}
+function hideAddItemBox() {
+  document.getElementById('addItemBox').style.display = 'none';
+}
+
+function addItem() {
+
 }
